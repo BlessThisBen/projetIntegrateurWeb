44f5091bf3e4ddc4c8bed42c542c2280b05d10d8
BlessThisBen <104938382+BlessThisBen@users.noreply.github.com>
2023-12-07 13:37:17 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+</main>
+
+<footer>
+    <p>Copyright @ Philippe V. et Benoît B. 2023</p>
+    <p>Dronica & Co.</p>
+</footer>
+
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,43 @@
+<?php 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+} 
+
+//include_once("inc/autoloader.php");
+//include_once("classe/PDOFactory.php");
+
+//$bdd = PDOFactory::getMySQLConnection();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr-CA">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Accueil</title>
+    <script src="js/script.js" defer></script>
+</head>
+<body>
+    <main>
+
+        <nav class="menu-utilisateur">
+            <ul>
+                <li><a href="panier.php">Panier</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+            </ul>
+        </nav>
+
+        <img src="img/banniereDesktop.png" alt="Bannière Dronica">
+
+        <nav class="menu-principal">
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="drones.php">Drones</a></li>
+                <li><a href="fournisseurListe.php">Fournisseurs</a></li>
+            </ul>
+        </nav>
+    
+    
